596cf8a2e14d7c47733fbecbdef397fd5479cf3b
Viraj Samson <virajsamson098@gmail.com>
2020-08-07 10:56:53 -0500

    email

--- Flaskblog/__init__.py
+++ Flaskblog/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 
 app = Flask(__name__)
@@ -12,5 +15,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'  # set the login route
 login_manager.login_message_category = 'info' # for flash msg
+app.config['MAIL_SERVER'] = 'stmp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+mail = Mail(app)
 
 from Flaskblog import routes
