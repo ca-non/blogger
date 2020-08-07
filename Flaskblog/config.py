89ef5244b699b69da4050f9e829d775fc9877dad
Viraj Samson <virajsamson098@gmail.com>
2020-08-07 14:36:09 -0500

    added blueprints

---
+++ Flaskblog/config.py
@@ -0,0 +1,11 @@
+
+class Config:
+	SECRET_KEY= '7d8d14c936780f6c7eb570f3d76f082a'
+	SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'
+	MAIL_SERVER= 'stmp.gmail.com'
+	MAIL_PORT= 465
+	MAIL_USE_TLS= False
+	MAIL_USE_SSL= True
+	MAIL_USERNAME= ''
+	MAIL_PASSWORD= ''
+    
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from Flaskblog import app
+from Flaskblog import create_app
+
+app = create_app() # using config class as default
 
 if __name__ == '__main__':
 	app.run(debug=True)
